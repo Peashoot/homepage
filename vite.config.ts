6fa7f13583c522367ee3f1cb12995c91c18c9e85
xinlei.wang <xinlei.wang@aconlab.com.cn>
2025-07-22 17:17:24 +0800

    单页面支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [vue(),
+  legacy({
+    targets: ['ie>=11'], // 支持旧版浏览器
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  })],
   build: {
     rollupOptions: {
       output: {
